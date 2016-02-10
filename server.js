f8227ddc88149d4499ebe12b9ee6fec86713c215
Sriram <trnsriram@gmail.com>
2016-02-10 10:09:46 +0530

    starting out with deepstream.io real-time-push

---
+++ server.js
@@ -0,0 +1,14 @@
+/**
+ * Created by sriram on 9/2/16.
+ */
+
+var DeepstreamServer = require( 'deepstream.io' ),
+    server = new DeepstreamServer();
+
+// Optionally you can specify some settings, a full list of which
+// can be found here http://deepstream.io/docs/deepstream.html
+server.set( 'host', 'localhost' );
+server.set( 'port', 6020 );
+
+// start the server
+server.start();
